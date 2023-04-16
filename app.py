db0ecfcb40244812b5199b362f26e3f23a37e43c
shaolun77 <116580308+shaolun77@users.noreply.github.com>
2023-04-16 13:07:50 -0400

    add os

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ from werkzeug.exceptions import Unauthorized
 
 from models import connect_db, db, User, Feedback
 from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask-feedback"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "shhhhh"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
